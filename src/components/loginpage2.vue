16533488bb6534cffd344a792721b3fd5d726009
Charlly234 <sikaicharlotte@gmail.com>
2017-12-20 18:29:14 +0100

    and a little more

--- src/components/loginpage2.vue
+++ src/components/loginpage2.vue
@@ -28,6 +28,47 @@
       <b-button type="submit" variant="primary">Submit</b-button>
       
     </b-form>
+
+
+
+<h2>Sign Up</h2>
+    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+      <b-form-group id="exampleInputGroup1"
+                    label="Name:"
+                    label-for="exampleInput1">
+        <b-form-input id="exampleInput1"
+                      type="text"
+                      v-model="form.name"
+                      required
+                      placeholder="Enter name">
+        </b-form-input>
+      </b-form-group>
+      <b-form-group id="exampleInputGroup1"
+                    label="Email address:"
+                    label-for="exampleInput1"
+                    description="You can trust us with your email.">
+        <b-form-input id="exampleInput1"
+                      type="email"
+                      v-model="form.email"
+                      required
+                      placeholder="Enter email">
+        </b-form-input>
+      </b-form-group>
+      <b-form-group id="exampleInputGroup2"
+                    label="Password:"
+                    label-for="exampleInput2">
+        <b-form-input id="exampleInput2"
+                      type="password"
+                      v-model="form.password"
+                      required
+                      placeholder="Enter password">
+        </b-form-input>
+      </b-form-group>
+      
+      
+      <b-button type="submit" variant="primary">Submit</b-button>
+      
+    </b-form>
   </div>
 </template>
 
@@ -51,11 +92,11 @@ export default {
     },
     onReset (evt) {
       evt.preventDefault();
-      /* Reset our form values */
+    
       this.form.email = '';
       this.form.password = '';
       
-      /* Trick to reset/clear native browser form validation state */
+     
       this.show = false;
       this.$nextTick(() => { this.show = true });
     }
@@ -70,6 +111,11 @@ export default {
 label{
     display: block;
     margin: 20px 0 10px;
+}
+input[type="text"], textarea{
+    display: block;
+    width: 20%;
+    padding: 8px;
 }
 input[type="email"], textarea{
     display: block;

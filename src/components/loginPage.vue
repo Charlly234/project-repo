72f06c68cc8d5e3118eefef296ec5a8d4b7468ed
Charlly234 <sikaicharlotte@gmail.com>
2017-12-21 19:25:53 +0100

    tried this out three times......rubbish all the way

--- src/components/loginPage.vue
+++ src/components/loginPage.vue
@@ -1,55 +1,4 @@
 <template>
-<<<<<<< HEAD
-   <div> 
-     <h2>Login</h2>
-     <form> 
-     <label>Email </label>
-     <input type="text" required/>
-     <label>Password </label>
-     <input type="password" required />
-     </form>
-   </div>
-</template>
-
-<script>
-
-export default {
-  data(){
-      return{
-
-      }
-  },
-  methods:{
-
-  }
-
-}
-</script>
-
-<style> 
-#Login *{
-    box-sizing: border-box;
-}
-#Login{
-    margin: 20px auto;
-    max-width: 500px;
-}
-label{
-    display: block;
-    margin: 20px 0 10px;
-}
-input[type="text"], textarea{
-    display: block;
-    width: 20%;
-    padding: 8px;
-}
-input[type="password"], textarea{
-    display: block;
-    width: 20%;
-    padding: 8px;
-}
-</style>
-=======
   <b-container fluid>
     <b-row class="my-5">
       <b-container>
@@ -81,9 +30,61 @@ input[type="password"], textarea{
             <input type="submit" value="Submit" class="btn btn-primary">
             </b-form>
           </b-col>
-          <b-col md="8">
+          
+
+
+
+          <b-col md="4">
+            <h4>Explore!!!</h4>
+            <p>Lots of research materials to look through, final year projects and other student projects alike. What's more exciting?? You can become an author!!!! Don't miss the chance to express your ingenuity. Explore topics, upload yours join a research cluster or create yours...all for FREE!! Sign up NOW!!</p>
+            
+            
+            <p>If you're already a member...welcome back! Enjoy!!</p>
+
+          </b-col>  
+
+         <b-col md="4">
             <h2>Signup</h2>
+            <b-form @submit.prevent="login">
+              <b-form-group 
+              label="Email" 
+              label-for="email" 
+              breakpoint="md" 
+              :label-cols="4"
+              invalid-feedback="Sorry that doesn't seem right"
+              :state="!errors.has('email')">
+                <b-form-input id="email" name="email" v-validate="{required: true, email: true}" type="email" v-model="email"></b-form-input>
+              </b-form-group>
+
+              
+             <b-form-group
+             label="Name"
+             label-for="name"
+             breakpoint="md"
+             :label-cols="4"
+             >
+               <b-form-input v-model="name" name="name" v-validate="{required: true}" type="text"></b-form-input>
+             </b-form-group> 
+              
+              
+              
+              <b-form-group
+              label="Password"
+              label-for="password"
+              breakpoint="md"
+              :label-cols="4"
+              invalid-feedback="That doesn't seem like a password"
+              :state="!errors.has('password')"
+              >
+                <b-form-input v-model="password" name="password" v-validate="{required: true}" type="password"></b-form-input>
+              </b-form-group>
+
+
+            <input type="submit" value="Submit" class="btn btn-primary">
+            </b-form>
           </b-col>
+          
+
         </b-row>
       </b-container>
     </b-row>
@@ -135,4 +136,3 @@ input[type="password"], textarea{
   }
 
 </script>
->>>>>>> 996221d4050570e3e3db7738608f212d130bc859
